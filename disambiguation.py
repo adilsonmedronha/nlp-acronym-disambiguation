535b8d6e764f5788a26c3c0a8f3279c9fb911032
Adilson Medronha <adilsonmedronha@Adilsons-MacBook-Air.local>
2023-11-28 22:28:19 -0300

    update

--- disambiguation.py
+++ disambiguation.py
@@ -6,11 +6,6 @@ import pandas as pd
 
 
 
-@lru_cache()
-def get_acronyms_dataframe():
-    return pd.read_pickle("data/synthetic_wemb_MiniLM_L6_v2.pkl")
-
-
 def jaccard_similarity(list1, list2):
     set1 = set(list1)
     set2 = set(list2)
@@ -38,17 +33,24 @@ def jaccard_based(df_sintatic, results):
 
 def get_acronym(sentence: str) -> str:
     pattern = r'\b[A-Z]+\d?[&\/]?[A-Z]+\b|\b[A-Z]{2,}(?:s\b)?'
-
     return re.findall(pattern, sentence)[0]
+
+@lru_cache()
+def get_acronyms_dataframe():
+    return pd.read_pickle("data/not_ambiguous.csv")
 
 def is_ambiguous(acronym: str) -> bool:
     df_acronyms = get_acronyms_dataframe()
-
     return len(df_acronyms[df_acronyms['Acronym'] == acronym]) > 1
+
+def expanded_acronym(acronym):
+    df_acronyms = get_acronyms_dataframe()
+
+    df_acronyms[df_acronyms['Acronym'] == acronym]['Expansion']
+    return df_acronyms[df_acronyms['Acronym'] == acronym]['Expansion']
 
 def expand_sentence(sentence, acronym):
     df_acronyms = get_acronyms_dataframe()
-
     expansions_for_acronym = df_acronyms[df_acronyms['Acronym'] == acronym]['Expansion'].unique()
     return re.sub(re.escape(acronym), expansions_for_acronym[0], sentence)
 
@@ -56,34 +58,39 @@ def single_inference(sentence):
     acr_target = get_acronym(sentence)
     if not is_ambiguous(acr_target):
         return expand_sentence(sentence, acr_target) 
-    else:
-        sentence_emb = get_sentence_embedding(sentence)
-        acr_expanded_pred = classifier(sentence_emb, our_train)
-        return expand_sentence(sentence, acr_expanded_pred) 
+    # else:
+    #     sentence_emb = get_sentence_embedding(sentence)
+    #     acr_expanded_pred = classifier(sentence_emb, our_train)
+    #     return expand_sentence(sentence, acr_expanded_pred) 
         
 def embedding_based_only(df_sintatic, results):
     for acronym in df_sintatic["Acronym"].unique():
-        df_filtered = df_sintatic[df_sintatic["Acronym"] == acronym]
-        expansion_unique = df_filtered["Expansion"].unique()
-
-        # set of expanded embeddings of an specific acronym 
-        unique_embeddings = {
-            expansion: df_filtered[df_filtered["Expansion"] == expansion].iloc[0]["Expansion_Emb"]
-            for expansion in expansion_unique
-        }
-
-        for _, row in df_filtered.iterrows():
-            sentence_emb = row["Sentence_Emb"]
-            label = row["Expansion"]
-            predictions = {
-                resolution: cosine_similarity(sentence_emb, emb)[0][0]
-                for resolution, emb in unique_embeddings.items()
+        if not is_ambiguous(acronym):
+            expanded = expanded_acronym(acronym)
+            print(expanded)
+            results["Label"].append(expanded)
+            results["Prediction"].append(expanded)
+            results["True"].append(True)
+        else:
+            df_filtered = df_sintatic[df_sintatic["Acronym"] == acronym]
+            expansion_unique = df_filtered["Expansion"].unique()
+            # set of expanded embeddings of an specific acronym 
+            unique_embeddings = {
+                expansion: df_filtered[df_filtered["Expansion"] == expansion].iloc[0]["Expansion_Emb"]
+                for expansion in expansion_unique
             }
+            print(acronym)
 
-            argmax = max(predictions, key=predictions.get)
-            
-            results["Label"].append(label)
-            results["Prediction"].append(argmax)
-            results["True"].append(label == argmax)
+            for _, row in df_filtered.iterrows():
+                sentence_emb = row["Sentence_Emb"]
+                label = row["Expansion"]
+                predictions = {
+                    resolution: cosine_similarity(sentence_emb, emb)[0][0]
+                    for resolution, emb in unique_embeddings.items()
+                }
+                argmax = max(predictions, key=predictions.get)
+                results["Label"].append(label)
+                results["Prediction"].append(argmax)
+                results["True"].append(label == argmax)
 
     return results
