acce64258f185dfea806a9923759f4492b3333cf
Adilson Medronha <adilsonmedronha@Adilsons-MacBook-Air.local>
2023-11-29 01:05:48 -0300

    updates

--- disambiguation.py
+++ disambiguation.py
@@ -3,7 +3,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from create_embeddings import get_sentence_embedding
 import re
 import pandas as pd
-
+import numpy as np
 
 
 def jaccard_similarity(list1, list2):
@@ -39,6 +39,10 @@ def get_acronym(sentence: str) -> str:
 def get_acronyms_dataframe():
     return pd.read_csv("data/acronyms_only.csv")
 
+@lru_cache()
+def get_dataset_dataframe():
+    return pd.read_pickle("data/synthetic_wemb_MiniLM_L6_v2.pkl")
+
 def is_ambiguous(acronym: str) -> bool:
     df_acronyms = get_acronyms_dataframe()
 
@@ -51,45 +55,49 @@ def expanded_acronym(acronym):
 def expand_sentence(sentence, acronym):
     df_acronyms = get_acronyms_dataframe()
     expansions_for_acronym = df_acronyms[df_acronyms['Acronym'] == acronym]['Expansion'].unique()
-    return re.sub(re.escape(acronym), f" >>[{expansions_for_acronym[0]}]<< ", sentence)
+    expanded_acr = expansions_for_acronym[0] if len(expansions_for_acronym) > 0 else expansions_for_acronym
+    return re.sub(re.escape(acronym), f" >>[{expanded_acr}]<< ", sentence)
+
 
 def single_inference(sentence):
     acr_target = get_acronym(sentence)
     if not is_ambiguous(acr_target):
-        return expand_sentence(sentence, acr_target) 
-    # else:
-    #     sentence_emb = get_sentence_embedding(sentence)
-    #     acr_expanded_pred = classifier(sentence_emb, our_train)
-    #     return expand_sentence(sentence, acr_expanded_pred) 
+        return expand_sentence(sentence, acr_target)
+    else:
+        sentence_emb = np.array(get_sentence_embedding(sentence))
+        our_dataset = get_dataset_dataframe()
+        unique_embeddings = {}
+        df_filtered = our_dataset[our_dataset["Acronym"] == acr_target]
+        for expansion in df_filtered["Expansion"].unique():
+            expansion_emb = np.array(df_filtered[df_filtered["Expansion"] == expansion].iloc[0]["Expansion_Emb"])
+            unique_embeddings[expansion] = expansion_emb
+
+        predictions = {
+            expansion: cosine_similarity(sentence_emb.reshape(1, -1), emb.reshape(1, -1))[0][0]
+            for expansion, emb in unique_embeddings.items()
+        }
+        acr_expanded_pred = max(predictions, key=predictions.get)
+        print(acr_expanded_pred)
+        return expand_sentence(sentence, acr_expanded_pred)
         
 def embedding_based_only(df_sintatic, results):
     for acronym in df_sintatic["Acronym"].unique():
-        if not is_ambiguous(acronym):
-            expanded = expanded_acronym(acronym)
-            # print(expanded)
-            # results["Label"].append(expanded)
-            # results["Prediction"].append(expanded)
-            # results["True"].append(True)
-            continue
-        else:
-            df_filtered = df_sintatic[df_sintatic["Acronym"] == acronym]
-            expansion_unique = df_filtered["Expansion"].unique()
-            # set of expanded embeddings of an specific acronym 
-            unique_embeddings = {
-                expansion: df_filtered[df_filtered["Expansion"] == expansion].iloc[0]["Expansion_Emb"]
-                for expansion in expansion_unique
+        df_filtered = df_sintatic[df_sintatic["Acronym"] == acronym]
+        expansion_unique = df_filtered["Expansion"].unique()
+        # set of expanded embeddings of an specific acronym 
+        unique_embeddings = {
+            expansion: df_filtered[df_filtered["Expansion"] == expansion].iloc[0]["Expansion_Emb"]
+            for expansion in expansion_unique
+        }
+        for _, row in df_filtered.iterrows():
+            sentence_emb = row["Sentence_Emb"]
+            label = row["Expansion"]
+            predictions = {
+                resolution: cosine_similarity(sentence_emb, emb)[0][0]
+                for resolution, emb in unique_embeddings.items()
             }
-
-            for _, row in df_filtered.iterrows():
-                sentence_emb = row["Sentence_Emb"]
-                label = row["Expansion"]
-                predictions = {
-                    resolution: cosine_similarity(sentence_emb, emb)[0][0]
-                    for resolution, emb in unique_embeddings.items()
-                }
-                argmax = max(predictions, key=predictions.get)
-                results["Label"].append(label)
-                results["Prediction"].append(argmax)
-                results["True"].append(label == argmax)
-
+            argmax = max(predictions, key=predictions.get)
+            results["Label"].append(label)
+            results["Prediction"].append(argmax)
+            results["True"].append(label == argmax)
     return results
